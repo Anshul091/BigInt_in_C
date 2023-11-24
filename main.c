52a2ce5565edf5893b84fedcdca31813dc4ecbef
Anshul Mittal <mittalanshul091@gmail.com>
2023-11-24 16:05:38 +0530

    Update commit_messages.txt

--- main.c
+++ main.c
@@ -1462,3 +1462,12 @@ prompt:
         {
             long long int a1, a2, b1, b2;
             printf("Enter the real part of first number: ");
+            scanf("%lld", &a1);
+            printf("Enter the imaginary part of first number: ");
+            scanf("%lld", &a2);
+            printf("Enter the real part of second number: ");
+            scanf("%lld", &b1);
+            printf("Enter the imaginary part of second number: ");
+            scanf("%lld", &b2);
+            Complex a = new_Complex();
+            Complex b = new_Complex();
